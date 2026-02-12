0c6e91c983c446800edac99d95efad61f9169aac
TuvAAAA <tuva.tenghall@elev.ga.ntig.se>
2026-02-12 09:38:48 +0100

    urm jag har kommit någon vart

---
+++ test1.py
@@ -0,0 +1,20 @@
+# en sträng med alla bokstäver i det svenska alfabetet
+alfabet = "abcdefghijklmnopqrstuvwxyzåäö"
+ny = list(alfabet)
+
+# meddelandet som ska krypteras
+meddelande = ["hej"]
+me = list(meddelande)
+
+# nyckeln är antalet positioner varje bokstav ska flyttas
+nyckel = 1
+# variabel för att lagra det krypterade meddelandet
+resultat = ""
+
+oord = ny.find(nyckel,)
+
+# UPPDRAG: Skriv en loop som skriver ut "ifk"
+# Tips: Använd .index() och []
+ord = [oord]
+
+print(ord)
---
+++ test2.py
@@ -0,0 +1,7 @@
+# Source - https://stackoverflow.com/q/15418561
+# Posted by user1103294
+# Retrieved 2026-02-12, License - CC BY-SA 3.0
+
+string = 'This is a string, with words!'
+string.split(" ")
+['This', 'is', 'a', 'string,', 'with', 'words!']
