5395de688a83786206a189c58925198f4d83ec60
TuvAAAA <tuva.tenghall@elev.ga.ntig.se>
2026-02-13 09:37:18 +0100

    yuh

--- test1.py
+++ test1.py
@@ -1,20 +1,31 @@
 # en sträng med alla bokstäver i det svenska alfabetet
 alfabet = "abcdefghijklmnopqrstuvwxyzåäö"
-ny = list(alfabet)
+alfabet == list(alfabet)
 
 # meddelandet som ska krypteras
 meddelande = ["hej"]
-me = list(meddelande)
+m = list(meddelande)
+
 
 # nyckeln är antalet positioner varje bokstav ska flyttas
 nyckel = 1
 # variabel för att lagra det krypterade meddelandet
 resultat = ""
 
-oord = ny.find(nyckel,)
-
 # UPPDRAG: Skriv en loop som skriver ut "ifk"
 # Tips: Använd .index() och []
-ord = [oord]
 
-print(ord)
+bokstav = "h"
+
+le = alfabet.index(bokstav)
+
+ny_bokstav = le + nyckel
+
+print(alfabet[ny_bokstav])
+
+meddelande-1 == meddelande
+
+
+
+
+
