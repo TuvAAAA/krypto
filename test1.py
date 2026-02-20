6d7b942a202b7a417ac16e81cfb1d6844aa34fb1
TuvAAAA <tuva.tenghall@elev.ga.ntig.se>
2026-02-20 09:39:14 +0100

    yippie, jag försvinner nu

--- test1.py
+++ test1.py
@@ -1,31 +1,41 @@
 # en sträng med alla bokstäver i det svenska alfabetet
+nyckel = 3
+
 alfabet = "abcdefghijklmnopqrstuvwxyzåäö"
-alfabet == list(alfabet)
 
-# meddelandet som ska krypteras
-meddelande = ["hej"]
-m = list(meddelande)
-
-
-# nyckeln är antalet positioner varje bokstav ska flyttas
-nyckel = 1
-# variabel för att lagra det krypterade meddelandet
-resultat = ""
-
-# UPPDRAG: Skriv en loop som skriver ut "ifk"
-# Tips: Använd .index() och []
-
-bokstav = "h"
-
-le = alfabet.index(bokstav)
-
-ny_bokstav = le + nyckel
-
-print(alfabet[ny_bokstav])
-
-meddelande-1 == meddelande
+m = "åäö"
 
 
 
+for bokstav in m:
 
+    # kolla om ny plats är längre än alfabetet, om det är det måste vi ta bort längden av alfabetet.   
+    plats_bokstav = alfabet.index(bokstav)
+    ny_plats = plats_bokstav + nyckel
+
+    if ny_plats >= 29:
+
+        ny_plats = ny_plats - 29
+
+        ny_bokstav = alfabet[ny_plats]
+    
+        print(ny_bokstav)
+        
+    else:
+        ny_bokstav = alfabet[ny_plats]
+        print(ny_bokstav)      
+
+    
+
+ # meddelandet som ska krypteras
+
+    #m = list(m)
+
+
+    # nyckeln är antalet positioner varje bokstav ska flyttas
+
+    # variabel för att lagra det krypterade meddelandet
+
+    # UPPDRAG: Skriv en loop som skriver ut "ifk"
+    # Tips: Använd .index() och []
 
